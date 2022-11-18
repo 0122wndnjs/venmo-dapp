626c8e80217221733ea5086e770c437d14837351
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-18 11:45:34 +0900

    writing first function

--- backend/contracts/Transactions.sol
+++ backend/contracts/Transactions.sol
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: MIT
-
+pragma solidity ^0.8.15;
 
 contract Transactions {
+    // Number of transactions
+    uint256 transactionCount;
 
+    // This will be an event that will be fired when a transaction is made
+    event Transfer(address from, address receiver, uint amount, string message, uint timestamp);
+
+    // Struc of the transactions that we will be storing on chain
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+    }
+
+    // Array of transactions 
+    TransferStruct[] transactions;
+
+    // Fuction to add a transaction to the blockchain with our struct
+    function addToBlockchain(address payable receiver, uint amount, string memory message) public {
+        transactionCount += 1;
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp));
+
+        // emit the event we created earlier
+        emit Transfer(msg.sender, receiver, amount, message, block.timestamp);
+    }
 }
