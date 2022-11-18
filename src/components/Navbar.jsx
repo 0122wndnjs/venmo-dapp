41522d9ef7abdf42041b8a38a5ea09d52076adc3
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-18 15:54:19 +0900

    adding context file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,42 @@
 import styles from "../styles/Navbar.module.css";
-import { ChevronDownIcon } from '@heroicons/react/outline'
+import { ChevronDownIcon } from "@heroicons/react/outline";
+import { TransactionContext } from "../context/context";
+import { useContext } from "react";
 
 const Navbar = () => {
+  const { currentAccount, connectWallet } = useContext(TransactionContext);
   return (
     <nav className={styles.navigationContainer}>
       <div className={styles.container}>
         <div className={styles.logoContainer}>
-          <img src="../assets/venmo-logo.svg" alt="Venmo Logo" />
+          <img
+            src="../assets/venmo-logo.svg"
+            alt="Venmo Logo"
+            className={styles.logoImage}
+          />
         </div>
-        <div className={styles.actionsContainer}>
-          <p>
-            Hello, <span className={styles.accentColor}>UserAddress!</span> 👋
-          </p>
-          <ChevronDownIcon className={styles.arrowDownIcon} />
-          <div className={styles.avatarContainer}>
-            <img className={styles.avatarImage} src='https://yeeqiang.me/avatar.jpeg' alt='' />
-          </div>
-        </div>
+        {currentAccount ? (
+          () => {
+            <div className={styles.actionsContainer}>
+              <p>
+                Hello, <span className={styles.accentColor}>UserAddress!</span>{" "}
+                👋
+              </p>
+              <ChevronDownIcon className={styles.arrowDownIcon} />
+              <div className={styles.avatarContainer}>
+                <img
+                  className={styles.avatarImage}
+                  src="https://yeeqiang.me/avatar.jpeg"
+                  alt=""
+                />
+              </div>
+            </div>;
+          }
+        ) : (
+          <button className={styles.connectBtn} onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
       </div>
     </nav>
   );
