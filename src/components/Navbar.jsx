7f754d88daf067e92085c013306bee0eddff0c04
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-18 15:22:21 +0900

    adding navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,25 @@
-const Navbar = () => {
-  return <nav></nav>
-}
+import styles from "../styles/Navbar.module.css";
+import { ChevronDownIcon } from '@heroicons/react/outline'
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <nav className={styles.navigationContainer}>
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>
+          <img src="../assets/venmo-logo.svg" alt="Venmo Logo" />
+        </div>
+        <div className={styles.actionsContainer}>
+          <p>
+            Hello, <span className={styles.accentColor}>UserAddress!</span> 👋
+          </p>
+          <ChevronDownIcon className={styles.arrowDownIcon} />
+          <div className={styles.avatarContainer}>
+            <img className={styles.avatarImage} src='https://yeeqiang.me/avatar.jpeg' alt='' />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
